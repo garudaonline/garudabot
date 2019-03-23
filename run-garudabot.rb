0ea498f970e818013d63142d089ea0ae46ec48e3
Jason Williams <jw@jasonw.org.uk>
2019-03-23 16:44:19 +0000

    add version to log

--- run-garudabot.rb
+++ run-garudabot.rb
@@ -29,10 +29,14 @@ c_logger = config["Logger"]
 log = Logger.new(STDOUT)
 
 log.level = Logger.const_get(c_logger["level"])
+log.progname = VERSION
+
+log.unknown "Log level #{log.level}"
+
 log.debug config.inspect
 
 c_nexus = config["Nexus"] 
-nexus = Nexus.new(c_nexus["uid"] , c_nexus["code"]) 
+nexus = Nexus.new(c_nexus["uid"] , c_nexus["code"],log) 
 
 c_irc = config["Ashes_IRC"] 
 
