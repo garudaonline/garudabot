c89bfec1bdf3475ed754b10d48afb4cdca931384
Jason Williams <jw@jasonw.org.uk>
2019-03-25 19:42:11 +0000

    Fix item not found error message again becuase of Sid's RELENTLESS PEDANTRY

--- garudabot.rb
+++ garudabot.rb
@@ -108,7 +108,7 @@ class Garuda_bot < Ashes_IRC
 		end
 
 		if items.empty? then
-			reply = "No items found (they might be private knowledge)."
+			reply = "Nothing found - it might be private knowledge, sorry"
 		elsif items.length > 5 then
 			reply = "#{items.length} results: " + items.map { |i| i.to_s }.join(", ")
 			if reply.length > 300 then
